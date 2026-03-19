de43bfd41bc66ce04be50f314e0515b0cad3f4a1
Shale Ahammed Taspy <shaleahamedtaspi@gmail.com>
2026-03-19 22:09:14 +0600

    upper buttons function done

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ let interviewTotal=document.getElementById('interviewTotal')
 let rejectedTotal=document.getElementById('rejectedTotal')
 let allCardsSection=document.getElementById('allCards')
 let jobCardSection=document.getElementById('jobCards')
+let allBtn = document.getElementById('all-btn')
+let interviewBtn = document.getElementById('interview-btn')
+let rejectedBtn = document.getElementById('rejected-btn')
 
 const mainContainer=document.querySelector('main')
+
 
 
 function count(){
@@ -17,5 +21,21 @@ function count(){
 }
 count()
 
-let button = document.getElementById("button-interview")
+function toggleChange(id){
+    allBtn.classList.add('bg-gray-300', 'text-blue-800')
+    interviewBtn.classList.add('bg-gray-300', 'text-blue-800')
+    rejectedBtn.classList.add('bg-gray-300', 'text-blue-800')
+
+    allBtn.classList.remove('bg-blue-800', 'text-white')
+    interviewBtn.classList.remove('bg-blue-800', 'text-white')
+    rejectedBtn.classList.remove('bg-blue-800', 'text-white')
+
+    console.log(id)
+
+    const selected = document.getElementById(id)
+
+    selected.classList.remove('bg-gray-300','text-blue-800')
+    selected.classList.add('bg-blue-800','text-white')
+}
+
 
